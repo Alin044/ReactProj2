b85f786cfaf6a8d599f3f52c695c011fa3d8b4e4
Alin <alinalin03030@gmail.com>
2025-05-13 10:18:03 +0000

    auf

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return (
         <header className="header-component">
-            <img src="../globe.png" alt="globe"></img>
+            <img src="../images/globe.png" alt="globe" className="globe-Header"></img>
             <h1>my travel journal.</h1>
         </header>
     )
