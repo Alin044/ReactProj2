8a1a15d777c01556a09641ca177d3dfc96cbb700
Alin <alinalin03030@gmail.com>
2025-05-13 10:05:36 +0000

    vite project 2

---
+++ components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return (
+        <header className="header-component">
+            <img src="../globe.png" alt="globe"></img>
+            <h1>my travel journal.</h1>
+        </header>
+    )
+}
