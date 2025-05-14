876cd5a5b5088180acec748eb2b6233d0998de23
Alin <alinalin03030@gmail.com>
2025-05-14 12:30:39 +0000

    auf

---
+++ components/Entry.jsx
@@ -0,0 +1,21 @@
+
+export default function Entry(){
+    return (
+        <article className="contryInfoArticle">
+            <img src="../images/mountFuji.jpg" alt="mount Fuji" className="country-image"></img>
+            <div className="country-info">
+                <div className="country-info-header">
+                    <img src="../images/marker.png" alt="marker image"></img>
+                    <h2>JAPAN</h2>
+                    <h3>View on Google Maps</h3>
+                </div>
+                <h1>Mount Fuji</h1>
+                <h2 className="dates-article">12 Jan, 2023 - 24 Jan, 2023</h2>
+                <p>
+                    Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
+                </p>
+
+            </div>
+        </article>
+    )
+}
